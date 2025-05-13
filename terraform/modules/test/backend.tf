94af5fe64b0e15e7916ad9bed88fa3f68bf89283
jahidexe <46343754+jahidexe@users.noreply.github.com>
2025-05-13 22:16:36 +0100

    Adding tf files

--- terraform/modules/test/backend.tf
+++ terraform/modules/test/backend.tf
@@ -1,10 +1,12 @@
 terraform {
   backend "s3" {
-    bucket       = "myproject-dev-terraform-state"
-    key          = "terraform.tfstate"
-    region       = "eu-west-1"
-    encrypt      = true
+    # These values will be filled in by GitHub Actions
+    # bucket       = "myproject-dev-terraform-state"
+    # key          = "terraform.tfstate"
+    # region       = "eu-west-1"
+    # encrypt      = true
     kms_key_id   = "arn:aws:kms:eu-west-1:509399620336:key/99e51278-2f41-4b52-88f5-ef5db7d5fe94"
-    use_lockfile = true
+    # Changed from use_lockfile to dynamodb_table which is expected by the GitHub workflow
+    # use_lockfile = true
   }
 }
